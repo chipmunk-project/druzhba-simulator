dc818a528a19e3ae9c96e74f7799887ebe724cd0
Mike Wong <mdw362@nyu.edu>
2020-05-06 15:06:38 -0400

    Update build_dgen.sh

--- build_dgen.sh
+++ build_dgen.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-cd dgen && cargo build && cd ..
+cd dgen 
+cargo run simple ../example_alus/stateful_alus/raw.alu ../example_alus/stateless_alus/stateless_alu.alu 2 2 1 "0,1,2,3"  -o ../src/prog_to_run.rs
+cd ..
