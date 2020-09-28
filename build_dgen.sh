32ef68e29770bcbf9c056ed6aa7316129d3a5c37
Mike Wong <mdw362@nyu.edu>
2020-09-28 16:25:46 -0400

    Don't send stdout and stderr to null device in build_dgen

--- build_dgen.sh
+++ build_dgen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd dgen 
-cargo run simple ../example_alus/stateful_alus/raw.alu ../example_alus/stateless_alus/stateless_alu.alu 2 2 1 -c "0,1,2,3"  -o ../src/prog_to_run.rs >/dev/null 2>&1
+cargo run simple ../example_alus/stateful_alus/raw.alu ../example_alus/stateless_alus/stateless_alu.alu 2 2 1 -c "0,1,2,3"  -o ../src/prog_to_run.rs 
 cd ..
